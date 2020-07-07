d7f5b28f91ef9619c8a9549b354e0656fe91050c
lalit <lalitnagrath@gmail.com>
2020-07-07 23:40:24 +0530

    crud

--- index.php
+++ index.php
@@ -3,9 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
+<?php require_once 'process.php' ;?>
+<div class="row justify-content-center">
+<form action="process.php" method="POST"> 
+<div class="form-group">
+<input type="text" name="name" value="enter your name">
+</div>
+<div class="form-group">
+<input type="text" name="location" value="enter your Location">
+</div>
+<div class="form-group">
+
+<button type="submit" name="save">Save</button>
+</div>
+
+</div>
+</form>
     
 </body>
 </html>
---
+++ process.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// if(function_exists(mysql)){
+//     echo "we have mysql";
+// }else{
+//     echo"we dont have mysql";
+// }
+$mysql= new mysql('localhost','root','wipro@007','crud');
+//or die(mysqli_error($mysqli));
+
+
+
+?>
